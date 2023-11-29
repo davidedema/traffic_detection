e80826112d62c24cdf2871268d67efc34a7d36ab
davidedema <davide.demartini@studenti.unitn.it>
2023-11-30 00:05:15 +0100

    Added street cropping (not final version)

--- counting_vehicles.py
+++ counting_vehicles.py
@@ -10,9 +10,63 @@ CONTOUR_WIDTH = (70, 300)
 CONTOUR_HEIGHT = (70, 300)
 OFFSET_FOR_DETECTION = 8
 
+def extend_line(line, image_shape):
+    """
+    Extend a line across the entire image while maintaining its direction.
+    """
+    x1, y1, x2, y2 = line
+
+    # Calculate the slope of the line
+    slope = (y2 - y1) / (x2 - x1 + 1e-5)  # Adding a small value to avoid division by zero
+
+    # Extend the line to the image borders
+    extended_x1 = 0
+    extended_y1 = int(y1 - slope * (x1 - extended_x1))
+
+    extended_x2 = image_shape[1] - 1
+    extended_y2 = int(y2 - slope * (x2 - extended_x2))
+
+    return [extended_x1, extended_y1, extended_x2, extended_y2]
+
+# TODO: Now crop is not perfect, detect only right line
+def cropStreet(frame):
+    """
+    Create a mask for cropping the street
+    """
+    blurredFrame = cv2.GaussianBlur(frame, (3, 3), 0)
+    edges = cv2.Canny(blurredFrame, 50, 150, apertureSize=3)
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, minLineLength=300, maxLineGap=10)
+    meanRightLine = None
+    if lines is not None:
+        for line in lines:
+            extended_line = extend_line(line[0], frame.shape)
+            slope = (extended_line[3] - extended_line[1]) / (
+                extended_line[2] - extended_line[0] + 1e-5
+            )
+            if slope > 0.3:
+                if meanRightLine is None:
+                    meanRightLine = line[0]
+                else:
+                    meanRightLine = np.vstack([meanRightLine, extended_line])
+    # draw the lines
+    if meanRightLine is not None:
+        meanRightLine = np.mean(meanRightLine, axis=0).astype(int)
+    
+    # create a mask to crop the street
+    mask = np.zeros(frame.shape, dtype=np.uint8)
+    roi_corners = np.array([[(meanRightLine[0], meanRightLine[1]), (meanRightLine[2], meanRightLine[3]), (0,frame.shape[0])]], dtype=np.int32)
+    channel_count = frame.shape[2]
+    ignore_mask_color = (255,) * channel_count
+    cv2.fillPoly(mask, roi_corners, ignore_mask_color)
+    return mask
+
+    # return frame
+
 
 def extractBgAndFilter(frame, bg_subtractor) -> np.ndarray:
-    """Extract background and filter a frame"""
+    """
+    Extract background and filter a frame
+    """
 
     greyFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blurredFrame = cv2.GaussianBlur(greyFrame, (15, 15), 0)
@@ -29,22 +83,23 @@ def extractBgAndFilter(frame, bg_subtractor) -> np.ndarray:
 
 # TODO - improve this function
 
-
 def filterAndUnifyLines(lines, boundingBoxes):
-    """Filter lines that are inside bounding boxes and return a single average of all of them for each box"""
+    """
+    Filter lines that are inside bounding boxes and return a single average of all of them for each box
+    """
 
     linesInBoundingBoxes = {}
     filteredLines = np.empty((0, 2, 2), dtype=np.int32)
-    
+
     # for each bounding box find the lines that are inside it
-    for i,(x, y, w, h) in enumerate(boundingBoxes):
+    for i, (x, y, w, h) in enumerate(boundingBoxes):
         linesInBoundingBoxes[i] = []
         for line in lines:
             x1, y1 = line[0]
             x2, y2 = line[1]
             if x1 > x and x2 < (x + w) and y1 > y and y2 < (y + h):
                 linesInBoundingBoxes[i].append(line)
-                
+
     # for each bounding identified by a dict key find the average of all the points
     for boxId in linesInBoundingBoxes.keys():
         avgStartPoint = np.zeros(2)
@@ -56,14 +111,17 @@ def filterAndUnifyLines(lines, boundingBoxes):
 
         avgStartPoint = avgStartPoint / len(linesInBoundingBoxes[boxId])
         avgStopPoint = avgStopPoint / len(linesInBoundingBoxes[boxId])
-        filteredLines = np.vstack([filteredLines, np.array([[avgStartPoint, avgStopPoint]]).astype(int)])
+        filteredLines = np.vstack(
+            [filteredLines, np.array([[avgStartPoint, avgStopPoint]]).astype(int)]
+        )
 
     return filteredLines
+
 
 def scale_vector(p1, p2, scaling_factor):
     """
     Scale a vector defined by two points (p1 and p2) by a scaling factor.
-    
+
     Parameters:
     - p1: Tuple representing the coordinates (x, y) of the first point.
     - p2: Tuple representing the coordinates (x, y) of the second point.
@@ -76,18 +134,18 @@ def scale_vector(p1, p2, scaling_factor):
     x2, y2 = p2
     new_x = x1 + scaling_factor * (x2 - x1)
     new_y = y1 + scaling_factor * (y2 - y1)
-    
+
     return int(new_x), int(new_y)
 
 
 def draw_flow(img, img_bgr, flow, boundingBoxes, step=16):
+    """
+    Draw the optical flow vectors on the given image.
+    """
 
     h, w = img.shape[:2]
-    # TODO - improve this function (compute only the one that are inside the bounding boxes)
     y, x = np.mgrid[step / 2 : h : step, step / 2 : w : step].reshape(2, -1).astype(int)
     fx, fy = flow[y, x].T
-    fx = fx
-    fy = fy
 
     lines = np.vstack([x, y, x - fx, y - fy]).T.reshape(-1, 2, 2)
     lines = np.int32(lines)
@@ -98,14 +156,17 @@ def draw_flow(img, img_bgr, flow, boundingBoxes, step=16):
         start_point = tuple(line[1])
         end_point = tuple(line[0])
         end_point_rescaled = scale_vector(start_point, end_point, 5)
-        cv2.arrowedLine(img_bgr, start_point, end_point_rescaled, (0,255,0), 2, tipLength=0.3)
-
+        cv2.arrowedLine(
+            img_bgr, start_point, end_point_rescaled, (0, 255, 0), 2, tipLength=0.3
+        )
 
     return img_bgr
 
 
 def centerCoordinates(x, y, w, h) -> tuple:
-    """Calculate center coordinates of a bounding box"""
+    """
+    Calculate center coordinates of a bounding box
+    """
 
     x1 = int(w / 2)
     y1 = int(h / 2)
@@ -117,7 +178,9 @@ def centerCoordinates(x, y, w, h) -> tuple:
 
 
 def drawBoundingBoxes(contours, frame) -> list:
-    """Draw bounding boxes around detected vehicles and return their center coordinates"""
+    """
+    Draw bounding boxes around detected vehicles and return their center coordinates
+    """
 
     detectedVehicles = []
     boundingBoxes = []
@@ -143,7 +206,9 @@ def drawBoundingBoxes(contours, frame) -> list:
 
 
 def countVehicles(frame, detectedVehicles, vehicleCounter) -> int:
-    """Count vehicles that cross the counting line with a margin of error"""
+    """
+    Count vehicles that cross the counting line with a margin of error
+    """
 
     for x, y in detectedVehicles:
         if y < (COUNT_LINE_POS + OFFSET_FOR_DETECTION) and y > (
@@ -161,26 +226,28 @@ def countVehicles(frame, detectedVehicles, vehicleCounter) -> int:
 
 
 def process_video(videoCapture):
-    """Process video frame by frame and detect vehicles counting them"""
+    """
+    Process video frame by frame and detect vehicles counting them
+    """
 
     bg_subtractor = cv2.createBackgroundSubtractorKNN(history=100, detectShadows=False)
     vehicleCounter = 0
     ret, prev_frame = videoCapture.read()
     if ret:
+        mask = cropStreet(prev_frame)
         prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
 
     while True:
         ret, frame = videoCapture.read()
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+        masked_frame = cv2.bitwise_and(frame, mask)
         global COUNT_LINE_POS
         COUNT_LINE_POS = int((frame.shape[0] * 4 / 5))
 
         if ret == False:
             break
 
-        # TODO - improve background subtraction
-        filteredImage = extractBgAndFilter(frame, bg_subtractor)
+        filteredImage = extractBgAndFilter(masked_frame, bg_subtractor)
 
         contours, _ = cv2.findContours(
             filteredImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
@@ -206,11 +273,7 @@ def process_video(videoCapture):
             prev_gray, gray_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0
         )
         prev_gray = gray_frame
-
-        frame_with_flow = draw_flow(gray_frame, frame, flow, boundingBoxes)
-        cv2.imshow("Vehicles flows", frame_with_flow)
-        # cv2.imshow("Vehicles counter", frame)
-
+        cv2.imshow("Vehicles flows", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
